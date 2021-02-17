3cc99a8df7c5b04085f78860ab9f91ce1075c07b
Sam King <samuel.eric.king@gmail.com>
2021-02-17 16:11:18 -0600

    Condition 3 & Conclusions

--- Martingale_App.py
+++ Martingale_App.py
@@ -192,12 +192,12 @@ option = st.sidebar.selectbox(
 if option == 'Condition 1: Spin Until Bankrupt' or option == 'Condition 2: Fixed Number of Spins' or option == 'Condition 3: X-or-nothing':
     odds = st.sidebar.selectbox(
         'Choose a Win Probability:',
-        ('House Odds', 'True Flip')
+        ('House Odds (47.3%)', 'True Flip (50/50)')
     )
 
-if odds == "House Odds":
+if odds == "House Odds (47.3%)":
     p_win = house_odds
-elif odds == "True Flip":
+elif odds == "True Flip (50/50)":
     p_win = true_flip_odds
 
 if option == 'Home':
@@ -205,111 +205,116 @@ if option == 'Home':
     st.text('This application allows users to model the results of martingale gambling strategies.\n'
             'By adjusting inputs into the model and choosing from various experimental conditions,\n'
             'the user can trivially model and examine the expected results of a massive combination\n'
-            'of custom-specified designs.')
-    st.subheader('Introduction')
-    st.text('Martingaling is a betting strategy which has bettors double '
-            'their wager after each \n'
-            'successive loss, returning to a set starting wager after each '
-            'win. During each martingale\n'
-            'cycle, the bettor is attempting to win an amount equal to their'
-            ' starting wager. Consider\n'
-            'the following example showing a martingale strategy based '
-            'on a $10 starting bet:\n')
-    results = {'Spin': ['Spin 1', 'Spin 2', 'Spin 3', 'Spin 4', 'Spin 5',
-                        'Spin 6'],
-               'Martingale Cycle': ['Cycle 1/ Spin 1', 'Cycle 2/ Spin 1',
-                                    'Cycle 2/ Spin 2', 'Cycle 2/ Spin 3',
-                                    'Cycle 3/ Spin 1', 'Cycle 3/ Spin 2'],
-               'Result': ['Win', 'Lose', 'Lose', 'Win', 'Lose', 'Win'],
-               'Return': ['+10', '-10', '-20', '+40', '-10', '+20'],
-               'Total Profit': ['+10', '0', '-20', '+20', '+10', '+30'],
-               'Next Action': ['Start New Cycle', 'Double Bet',
-                               'Double Bet Again', 'Start New Cycle',
-                               'Double Bet', 'Start New Cycle']
-               }
-    df = pd.DataFrame(results)
-    df.set_index('Spin', inplace=True)
-    st.write(df.head(6))
-    st.text('The profit history of the of the above results can be graphed'
-            ' accordingly:')
+            'of custom-specified experimental designs.')
+    st.text('Use the sidebar to the left to navigate through the app, or see below for a brief user\n'
+            'guide and explanation of our methodology.')
+    if st.checkbox('What Is Martingaling?'):
+        st.text('Martingaling is a betting strategy which has bettors double '
+                'their wager after each \n'
+                'successive loss, returning to a set starting wager after each '
+                'win. During each martingale\n'
+                'cycle, the bettor is attempting to win an amount equal to their'
+                ' starting wager. Consider\n'
+                'the following example showing a martingale strategy based '
+                'on a $10 starting bet:\n')
+        results = {'Spin': ['Spin 1', 'Spin 2', 'Spin 3', 'Spin 4', 'Spin 5',
+                            'Spin 6'],
+                'Martingale Cycle': ['Cycle 1/ Spin 1', 'Cycle 2/ Spin 1',
+                                        'Cycle 2/ Spin 2', 'Cycle 2/ Spin 3',
+                                        'Cycle 3/ Spin 1', 'Cycle 3/ Spin 2'],
+                'Result': ['Win', 'Lose', 'Lose', 'Win', 'Lose', 'Win'],
+                'Return': ['+10', '-10', '-20', '+40', '-10', '+20'],
+                'Total Profit': ['+10', '0', '-20', '+20', '+10', '+30'],
+                'Next Action': ['Start New Cycle', 'Double Bet',
+                                'Double Bet Again', 'Start New Cycle',
+                                'Double Bet', 'Start New Cycle']
+                }
+        df = pd.DataFrame(results)
+        df.set_index('Spin', inplace=True)
+        st.write(df.head(6))
+        st.text('The profit history of the of the above results can be graphed'
+                ' accordingly:')
 
-    spin = [0, 1, 2, 3, 4, 5, 6]
-    profit = [0, 10, 0, -20, 20, 10, 30]
-    fig = plt.figure(figsize=(7, 3))
-    plt.xlabel('Spin')
-    plt.ylabel('Profit ($)')
-    plt.vlines(1, -20, 30, linestyles='--', colors='green',
-               label="Start New Cycle")
-    plt.vlines(4, -20, 30, linestyles='--', colors='green')
-    plt.vlines(6, -20, 30, linestyles='--', colors='green')
-    plt.legend(loc=2)
-    plt.plot(profit)
-    st.write(fig)
+        spin = [0, 1, 2, 3, 4, 5, 6]
+        profit = [0, 10, 0, -20, 20, 10, 30]
+        fig = plt.figure(figsize=(7, 3))
+        plt.xlabel('Spin')
+        plt.ylabel('Profit ($)')
+        plt.vlines(1, -20, 30, linestyles='--', colors='green',
+                label="Start New Cycle")
+        plt.vlines(4, -20, 30, linestyles='--', colors='green')
+        plt.vlines(6, -20, 30, linestyles='--', colors='green')
+        plt.legend(loc=2)
+        plt.plot(profit)
+        st.write(fig)
+        st.text('This general pattern of doubling wagers after each sucessive loss and return to a fixed\n'
+                'starting wager after each win is called a "martingale" strategy.')
+    if st.checkbox('User Guide'):
+        st.header('User Guide')
+        st.subheader('Running Your Martingale Experiment:')
+        st.text('Within the left sidebar, use the dropdown menu to select among the experimental\n'
+                'conditions described below. Within each condition, you will find various sliders\n'
+                'and dropdowns representing inputs into the experiment. In each experimental\n'
+                'condition, users will be asked to select:')
+        st.subheader('Win Probability')
+        st.text('This represents the probability of winning a given hand/ spin. "House Odds"\n'
+                'represents a 47.3% chance of winning a given hand/ spin, roughly the same\n'
+                'as a red/black bet at a casino roulette wheel. "True Flip" odds represent\n'
+                'a 50% chance of success, as one would expect to encounter gambling in a fair\n'
+                'game among friends.\n')
+        st.subheader('Number of Trials')
+        st.text('This slider allows users to choose the number of participants for which to\n'
+                'repeat the experiment. The example above represents a single trial. Each\n'
+                'trial is represented on the line chart (figure 1) with its own line. Running\n'
+                'the experiment with more trials will produce a more robust set of statistical\n'
+                'inferences, but is also likely to lead to more extreme outlier obseravtions.\n')
+        st.subheader('Starting Bankroll')
+        st.text('This slider gives users control over the amount of money with which each trial\n'
+                'begins. When this amount is lost, the trial will be considered bankrupt (aka ruin\n'
+                'scneario) and the trial will be over./n')
+        st.subheader('Starting Wager')
+        st.text('This slider allows users to control the starting wager for each martingale cycle.\n'
+                'This input is capped at 10% of the starting bankroll to allow for an adequate\n'
+                'implementation of the martingale strategy.\n'
+                )
+        st.subheader('Number of Spins (Condition 2 Only)')
+        st.text('In condition 2, this slider allows users to explore the effect of a stopping\n'
+                'criteria based on number of spins. Trials that do not go bankrupt will end\n'
+                'once this limit has been reached.\n'
+                )
+        st.subheader('Profit Goal (Condition 3 Only)')
+        st.text('In condition 3, this slider allows users to explore the effect of a stopping\n'
+                'criteria based on a profit goal, as represented by a multiple of the starting\n'
+                'bankroll. Trials that do not go bankrupt will end once this goal has been reached.')
+    
+    if st.checkbox('Methodology'):
+        st.header('Methodology')
+        st.text('The purpose of this application is to allow users to trivially explore the martingale\n'
+                'strategy under various conditions and inputs. Using repeated stochastic simulation, we\n'
+                'model the distribution of results of the martingale strategy within varied experimental\n'
+                'conditions and subject to the user-defined model inputs described above. By inputting\n'
+                'various assumptions, users are able to quickly run and re-run related experiments and \n'
+                'examine the impact of those assumptions on the distribution of results the strategy will\n'
+                'produce.\n')
+        st.subheader('Condition 1 - Spin Until Bankrupt')
+        st.text('This is the most straightforward condition, in which each trial (representing a bettor) \n'
+                'is allowed to martingale until bankruptcy. This condition is largely theoretical, in that\n'
+                'few bettors undertake a strategy intent on eventually going bankrupt. That aside, this\n'
+                'condition allows us to best understand the full distribution of outcomes associated with\n'
+                'a given martingale approach.\n')
 
-    st.header('User Guide')
-    st.subheader('Running Your Martingale Experiment:')
-    st.text('Within the left sidebar, use the dropdown menu to select among the experimental\n'
-            'conditions described below. Within each condition, you will find various sliders\n'
-            'and dropdowns representing inputs into the experiment. In each experimental\n'
-            'condition, users will be asked to select:')
-    st.subheader('Win Probability')
-    st.text('This represents the probability of winning a given hand/ spin. "House Odds"\n'
-            'represents a 47.3% chance of winning a given hand/ spin, roughly the same\n'
-            'as a red/black bet at a casino roulette wheel. "True Flip" odds represent\n'
-            'a 50% chance of success, as one would expect to encounter gambling in a fair\n'
-            'game among friends.\n')
-    st.subheader('Number of Trials')
-    st.text('This slider allows users to choose the number of participants for which to\n'
-            'repeat the experiment. The example above represents a single trial. Each\n'
-            'trial is represented on the line chart (figure 1) with its own line. Running\n'
-            'the experiment with more trials will produce a more robust set of statistical\n'
-            'inferences, but is also likely to lead to more extreme outlier obseravtions.\n')
-    st.subheader('Starting Bankroll')
-    st.text('This slider gives users control over the amount of money with which each trial\n'
-            'begins. When this amount is lost, the trial will be considered bankrupt (aka ruin\n'
-            'scneario) and the trial will be over./n')
-    st.subheader('Starting Wager')
-    st.text('This slider allows users to control the starting wager for each martingale cycle.\n'
-            'This input is capped at 10% of the starting bankroll to allow for an adequate\n'
-            'implementation of the martingale strategy.\n'
-            )
-    st.subheader('Number of Spins (Condition 2 Only)')
-    st.text('In condition 2, this slider allows users to explore the effect of a stopping\n'
-            'criteria based on number of spins. Trials that do not go bankrupt will end\n'
-            'once this limit has been reached.\n'
-            )
-    st.subheader('Profit Goal (Condition 3 Only)')
-    st.text('In condition 3, this slider allows users to explore the effect of a stopping\n'
-            'criteria based on a profit goal, as represented by a multiple of the starting\n'
-            'bankroll. Trials that do not go bankrupt will end once this goal has been reached.')
+        st.subheader('Condition 2 - Set Number of Spins')
+        st.text('In this condition, each trial is stopped once the bettor reaches a set number of spins\n'
+                'as defined by the user (via an an additional slider). This condition implements a\n'
+                'stopping criteria which is intended to act as a real-world constraint (e.g. 250 spins\n'
+                'may be used to represent a full day of martingaling at a casino roulette wheel -\n'
+                'assuming something on the order of 40 spins per hour.')
 
-    st.header('Methodology')
-    st.text('The purpose of this application is to allow users to trivially explore the martingale\n'
-            'strategy under various conditions and inputs. Using repeated stochastic simulation, we\n'
-            'model the distribution of results of the martingale strategy within varied experimental\n'
-            'conditions and subject to the user-defined model inputs described above. By inputting\n'
-            'various assumptions, users are able to quickly run and re-run related experiments and \n'
-            'examine the impact of those assumptions on the distribution of results the strategy will\n'
-            'produce.\n')
-    st.subheader('Condition 1 - Spin Until Bankrupt')
-    st.text('This is the most straightforward condition, in which each trial (representing a bettor) \n'
-            'is allowed to martingale until bankruptcy. This condition is largely theoretical, in that\n'
-            'few bettors undertake a strategy intent on eventually going bankrupt. That aside, this\n'
-            'condition allows us to best understand the full distribution of outcomes associated with\n'
-            'a given martingale approach.\n')
-
-    st.subheader('Condition 2 - Set Number of Spins')
-    st.text('In this condition, each trial is stopped once the bettor reaches a set number of spins\n'
-            'as defined by the user (via an an additional slider). This condition implements a\n'
-            'stopping criteria which is intended to act as a real-world constraint (e.g. 250 spins\n'
-            'may be used to represent a full day of martingaling at a casino roulette wheel -\n'
-            'assuming something on the order of 40 spins per hour.')
-
-    st.subheader('Condition 3 - Spin Until Profit Goal')
-    st.text('Similar to condition two, this condition attempts to implement a real-world stopping\n'
-            'criteria, in this case a martingaler who is attempting to achieve some profit goal\n'
-            'before stopping (e.g. the bettor wants to double/triple/10x their bankroll and then\n'
-            'stop).\n')
+        st.subheader('Condition 3 - Spin Until Profit Goal')
+        st.text('Similar to condition two, this condition attempts to implement a real-world stopping\n'
+                'criteria, in this case a martingaler who is attempting to achieve some profit goal\n'
+                'before stopping (e.g. the bettor wants to double/triple/10x their bankroll and then\n'
+                'stop).\n')
 
 elif option == 'Condition 1: Spin Until Bankrupt':
     st.header("Condition 1: Spin Until Bankrupt")
@@ -327,6 +332,7 @@ elif option == 'Condition 1: Spin Until Bankrupt':
                                  5, int(bankroll/10), 10, step=5)
     # Create first chart
     results = spin_til_bust(trials, bankroll, startBet)
+    left.subheader('Results')
     left.text('Figure 1 shows the profit history of trial\n'
               'in this experiment. Note that most trials\n'
               'end in bankruptcy fairly quickly, but a\n'
@@ -403,6 +409,8 @@ elif option == 'Condition 1: Spin Until Bankrupt':
     ax.yaxis.set_major_formatter(mtick.PercentFormatter())
     ax.set(xlabel='Peak Profit of Trial ($)', ylabel='Frequency of Outcome')
     right.pyplot(fig)
+    right.subheader('')
+    right.text('')
     right.text('Figure 2 shows the distribution of peak\n'
                'profits for each trial (with each red \n'
                'line on the rugplot representing a\n'
@@ -517,6 +525,10 @@ elif option == 'Condition 3: X-or-nothing':
             loserResults.append(i)
     winnerCount = len(winnerResults)
     loserCount = len(loserResults)
+    winnerTotals = [i[-1] for i in winnerResults] 
+    loserTotals = [i[-1] for i in loserResults]
+    total_won_c3 = sum(winnerTotals)
+    total_lost_c3 = sum(loserTotals)
     x = ['Winners', 'Losers']
     y = [winnerCount, loserCount]
     fig = plt.figure(figsize=(8, 6))
@@ -524,8 +536,41 @@ elif option == 'Condition 3: X-or-nothing':
     right.subheader('Proportion of Winners and Losers')
     plt.title('Figure 2: Proportion of Winners and Losers')
     right.pyplot(fig)
+
+    st.header('Results:')
+    st.text('The ' + str(len(winnerResults)) + ' winning trials won a total of $' + str(total_won_c3) + ' or $' + str(round(total_won_c3/len(winnerResults),2)) +' per winning trial.\n'
+            'The ' + str(len(loserResults)) + ' losing trials won a total of $' + str(total_lost_c3) + ' or $' + str(round(total_lost_c3/len(loserResults),2)) +' per losing trial.\n')
+    st.text('The total return of all trials in this experiment was $' + str(total_won_c3+total_lost_c3) + '.\n'
+            'The expected value of each trial is $' + str(round((total_won_c3+total_lost_c3)/trials,2)) + ' or ' + str(round((((total_won_c3+total_lost_c3)/trials)/bankroll)*100,2)) +'% of the starting bankroll.')
+
     # RESUME HERE
 elif option == "Conclusions":
     st.header('Conclusions')
+    st.subheader('General')
+    st.text('From this experiment, we can conclude that martingaling is an extremely high variance\n'
+            'strategy, with an asymmetric downside risk for participants. Further, the strategy does\n'
+            'not significantly alter a game\'s expectation from any other strategy. Martingaling is\n'
+            'not an effective risk-mitigation strategy, and those who try it for long enough will\n'
+            'eventually go bankrupt.')
+    st.text('If one is intent on martingaling, they will see relatively much better results if it\n'
+            'can be done in a "fair" game, or one in which the house does not have an inherent\n'
+            'advantage. That said, even in a fair game, martingale trials will still eventually\n'
+            'end in bankruptcy.')
+    st.subheader('Condition 1')
+    st.text('Condition 1 proved that, given a finite bankroll, a martingale strategy in a negative-\n'
+            'expectation game will inevitable end in bankruptcy.\n')
+    st.text('This condition also showed that adjusting odds to remove a house edge (e.g. a neutral-\n'
+            'expectation game) will produce a wider distribution of results and increase the expected\n'
+            'longevity of each individual trial')
+    st.subheader('Condition 2')
+    st.text('Condition 2 showed that more a more aggressive approach (either a longer trial-duration\n'
+            'or higher starting-bet:bankroll ratio) will result in worse performance for each trial,\n'
+            'as indicated by a lower returned expected value per trial.')
+    st.subheader('Condition 3')
+    st.text('Similar to condition 2, condition 3 showed that a more aggressive approach (either a more\n'
+            'ambitious stopping critera or starting-bet:bankroll ratio) will result in worse\n'
+            'performance for each trial, as indicated by a lower returned expected value per trial.')
+    
+
 else:
     st.write('Hmmm... this shouldn\'t be...')
