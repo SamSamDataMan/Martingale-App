a0d198687d54732d21a39bf453740947df110477
Sam King <samuel.eric.king@gmail.com>
2021-05-04 10:53:39 -0500

    Takeaways Condition 3

--- views/cond3.py
+++ views/cond3.py
@@ -66,3 +66,33 @@ def render_cond3(left, right, trials, results, bankroll):
         + "% of the"
         " starting bankroll."
     )
+
+    st.header("Takeaways From Condition 3")
+    
+    st.subheader("1. A more ambituous stopping criteria leads to worse results.")
+    st.text("By examining the results based on higher of lowe multiplier goals, we can see that,\n"
+            "intuitively, the more ambitous the goal, the smaller the proportion of trials reach\n"
+            "that goal. Similarly, the expected value of a given trial decreases proportionally as\n"
+            "the multiplier goal increases.\n"
+            )
+    
+    #st.subheader("2. Minimizing the starting bankroll produces widely varied results.")
+    #st.text("By minimizing the starting bankroll, bankruptcy becomes more likely over a given number\n"
+        # "of spins. However, the impact of those bankrupt trials is also minimized on the overall\n"
+        # "expected value. While minimizing number of spins, the expected values associated with this\n"
+        # "strategy may range from slightly positive to massively negative, depending on the\n"
+        # "observed number of bankrupt trials. However, by increasing the number of spins and holding\n"
+        # "the small bankroll constant, we see a familiar pattern in the proportion of losing and\n"
+        # "bankrupt trials growing, and the expected value of the strategy approaching -100%.")
+    
+    #st.subheader(
+        # "3. A large bankroll-to-starting wager ratio improves the proportion of winners but does not improve expected value.")
+    #st.text(
+        # "Over a given number of spins, it is much less likely to go bankrupt with a large bankroll\n"
+        # "relative to the starting wager. However, the downside risk of those bankruptcies has a\n"
+        # "larger impact on the expected value when they do occur. Likewise, the gradual nature of \n"
+        # "winnings associated with this strategy do not accumulate very quickly in comparison to the\n"
+        # "large starting bankroll. Thus, over a small number of spins, an experiment with this\n"
+        # "specification will produce a small positive or negative expected value. Over a larger\n"
+        # "number of spins, the same pattern emerges that expected value decreases in proportion to\n"
+        # "the trial duration.")
